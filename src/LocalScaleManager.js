492098b7323af1e6b99254610a3c0b0a4473349a
Tom Odell <Dark-Hippo@users.noreply.github.com>
2025-01-17 15:09:09 +0000

    Implementing scaling

--- src/LocalScaleManager.js
+++ src/LocalScaleManager.js
@@ -1,19 +1,22 @@
-LocalScaleManager.prototype = {
-    scaleSprite: function (sprite, availableSpaceWidth, availableSpaceHeight, padding, scaleMultiplier, isFullScale) {
+export class LocalScaleManager {
+    scaleSprite(sprite, availableSpaceWidth, availableSpaceHeight, padding, scaleMultiplier, isFullScale) {
         let scale = this.getSpriteScale(sprite.frame.width, sprite.frame.height, availableSpaceWidth, availableSpaceHeight, padding, isFullScale);
         sprite.setScale(scale * scaleMultiplier);
         return scale;
-    },
-    scaleSpriteTo: function (sprite, scale) {
+    }
+    
+    scaleSpriteTo (sprite, scale) {
         sprite.setScale(scale);
-    },
-    scaleText: function (sprite, availableSpaceWidth, availableSpaceHeight, padding, scaleMultiplier, isFullScale) {
+    }
+
+    scaleText (sprite, availableSpaceWidth, availableSpaceHeight, padding, scaleMultiplier, isFullScale) {
         let originalWidth = sprite.width;
         let originalHeight = sprite.height;
         let scale = this.getSpriteScale(originalWidth, originalHeight, availableSpaceWidth, availableSpaceHeight, padding, isFullScale);
         sprite.setScale(scale * scaleMultiplier);
-    },
-    getSpriteScale: function (spriteWidth, spriteHeight, availableSpaceWidth, availableSpaceHeight, minPadding, isFullScale) {
+    }
+    
+    getSpriteScale (spriteWidth, spriteHeight, availableSpaceWidth, availableSpaceHeight, minPadding, isFullScale) {
         let ratio = 1;
         let currentDevicePixelRatio = window.devicePixelRatio;
         // Sprite needs to fit in either width or height
@@ -28,5 +31,3 @@ LocalScaleManager.prototype = {
         return ratio * currentDevicePixelRatio;
     }
 };
-
-let localScaleManager = new LocalScaleManager;
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,46 +1,13 @@
 import { Scene } from 'phaser';
 import { GameOver } from './GameOver';
+import { LocalScaleManager } from '../LocalScaleManager';
 
 const PEG_SIZE = 60;
 const PEG_FRAME_EMPTY = 0;
 const PEG_FRAME_FULL = 1;
 const PEG_FRAME_SELECTED = 2;
 
-function LocalScaleManager() {
-}
-
-LocalScaleManager.prototype = {
-    scaleSprite: function (sprite, availableSpaceWidth, availableSpaceHeight, padding, scaleMultiplier, isFullScale) {
-        let scale = this.getSpriteScale(sprite.frame.width, sprite.frame.height, availableSpaceWidth, availableSpaceHeight, padding, isFullScale);
-        sprite.setScale(scale * scaleMultiplier);
-        return scale;
-    },
-    scaleSpriteTo: function (sprite, scale) {
-        sprite.setScale(scale);
-    },
-    scaleText: function (sprite, availableSpaceWidth, availableSpaceHeight, padding, scaleMultiplier, isFullScale) {
-        let originalWidth = sprite.width;
-        let originalHeight = sprite.height;
-        let scale = this.getSpriteScale(originalWidth, originalHeight, availableSpaceWidth, availableSpaceHeight, padding, isFullScale);
-        sprite.setScale(scale * scaleMultiplier);
-    },
-    getSpriteScale: function (spriteWidth, spriteHeight, availableSpaceWidth, availableSpaceHeight, minPadding, isFullScale) {
-        let ratio = 1;
-        let currentDevicePixelRatio = window.devicePixelRatio;
-        // Sprite needs to fit in either width or height
-        let widthRatio = (spriteWidth * currentDevicePixelRatio + 2 * minPadding) / availableSpaceWidth;
-        let heightRatio = (spriteHeight * currentDevicePixelRatio + 2 * minPadding) / availableSpaceHeight;
-        if (widthRatio > 1 || heightRatio > 1) {
-            ratio = 1 / Math.max(widthRatio, heightRatio);
-        } else {
-            if (isFullScale)
-                ratio = 1 / Math.max(widthRatio, heightRatio);
-        }
-        return ratio * currentDevicePixelRatio;
-    }
-};
-
-let localScaleManager = new LocalScaleManager;
+let localScaleManager = new LocalScaleManager();
 
 export class Game extends Scene
 {
